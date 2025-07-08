a80692204bae7706dd5d30642d380824ebb80329
Prachigoyal23 <goyalprachi2324@gmail.com>
2025-07-08 16:57:19 +0530

    Update the search functionality

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -156,79 +156,182 @@
 
 // -----------------------------------------------------------
 
+// import { useState } from 'react';
+// import './Homepage.css';
+// import { Link } from 'react-router-dom';
+
+// function HomePage({ sideNavbar }) {
+//     const options = ["All", "JavaScript", "React", "MongoDB", "MERN Stack", "Web Development", "HTML", "CSS", "Nodejs", "Expressjs", "FrontEnd", "BackEnd", "Fullstack Development", "Poetry"];
+//     const [selectedCategory, setSelectedCategory] = useState("All");
+
+//     return (
+//         <div className={sideNavbar ? "homePage" : "fullHomePage"}>
+//             <div className="homePage_options">
+//                 {options.map((item, index) => (
+//                     <div
+//                         key={index}
+//                         className={`homePage_option ${selectedCategory === item ? 'selected' : ''}`}
+//                         onClick={() => setSelectedCategory(item)}
+//                     >
+//                         {item}
+//                     </div>
+//                 ))}
+//             </div>
+
+//             <div className={sideNavbar ? "home_mainPage" : "home_mainPageWithoutLink"}>
+//                 {/* Dummy data inline */}
+//                 {[
+//                     {
+//                         id: 1,
+//                         title: "Learn JavaScript in 3 Hours",
+//                         category: "JavaScript",
+//                         thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+//                         duration: "3:10:47",
+//                         user: "User1",
+//                         likes: "3 Likes"
+//                     },
+//                     {
+//                         id: 2,
+//                         title: "React Crash Course",
+//                         category: "React",
+//                         thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+//                         duration: "2:45:12",
+//                         user: "User2",
+//                         likes: "5 Likes"
+//                     },
+//                     {
+//                         id: 3,
+//                         title: "CSS Basics",
+//                         category: "CSS",
+//                         thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+//                         duration: "1:15:30",
+//                         user: "User3",
+//                         likes: "2 Likes"
+//                     },
+//                     {
+//                         id: 4,
+//                         title: "HTML Beginner Guide",
+//                         category: "HTML",
+//                         thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+//                         duration: "1:00:00",
+//                         user: "User4",
+//                         likes: "4 Likes"
+//                     },
+//                     {
+//                         id: 5,
+//                         title: "Poetry Collection",
+//                         category: "Poetry",
+//                         thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+//                         duration: "0:45:00",
+//                         user: "PoetX",
+//                         likes: "10 Likes"
+//                     }
+//                 ]
+//                     .filter(video => selectedCategory === "All" || video.category === selectedCategory)
+//                     .map(video => (
+//                         <Link to={`/watch/${video.id}`} className="youtube_Video" key={video.id}>
+//                             <div className="youtube_thumbnailBox">
+//                                 <img src={video.thumbnail} alt="Thumbnail" className="youtube_thumbnailPic" />
+//                                 <div className="youtube_timingThumbnail">{video.duration}</div>
+//                             </div>
+
+//                             <div className="youtubeTitleBox">
+//                                 <div className="youtubeTitleBoxProfile">
+//                                     <img src="https://www.shutterstock.com/image-photo/close-portrait-smiling-30s-caucasian-260nw-2246095709.jpg" alt="Profile" className="youtube_thumbnail_Profile" />
+//                                 </div>
+
+//                                 <div className="youtubeTitleBox_Title">
+//                                     <div className="youtube_videoTitle">{video.title}</div>
+//                                     <div className="youtube_channelName">{video.user}</div>
+//                                     <div className="youtubeVideo_views">{video.likes}</div>
+//                                 </div>
+//                             </div>
+//                         </Link>
+//                     ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default HomePage;
+
+
+// --------------------------------------------------------------------------------------------
+
 import { useState } from 'react';
 import './Homepage.css';
 import { Link } from 'react-router-dom';
 
 function HomePage({ sideNavbar }) {
     const options = ["All", "JavaScript", "React", "MongoDB", "MERN Stack", "Web Development", "HTML", "CSS", "Nodejs", "Expressjs", "FrontEnd", "BackEnd", "Fullstack Development", "Poetry"];
+
     const [selectedCategory, setSelectedCategory] = useState("All");
+
+    // Dummy data (can be replaced with real API/video list)
+    const videos = [
+        {
+            id: 1,
+            title: "Learn JavaScript in 3 Hours",
+            category: "JavaScript",
+            thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+            duration: "3:10:47",
+            user: "User1",
+            likes: "3 Likes"
+        },
+        {
+            id: 2,
+            title: "React Crash Course",
+            category: "React",
+            thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+            duration: "2:45:12",
+            user: "User1",
+            likes: "5 Likes"
+        },
+        {
+            id: 3,
+            title: "Introduction to HTML",
+            category: "HTML",
+            thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+            duration: "1:30:00",
+            user: "User1",
+            likes: "7 Likes"
+        },
+        {
+            id: 4,
+            title: "CSS for Beginners",
+            category: "CSS",
+            thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
+            duration: "1:50:00",
+            user: "User1",
+            likes: "2 Likes"
+        },
+        // ...add more items if needed
+    ];
+
+    // Filter videos based on selected category
+    const filteredVideos = selectedCategory === "All"
+        ? videos
+        : videos.filter(video => video.category === selectedCategory);
 
     return (
         <div className={sideNavbar ? "homePage" : "fullHomePage"}>
             <div className="homePage_options">
-                {options.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`homePage_option ${selectedCategory === item ? 'selected' : ''}`}
-                        onClick={() => setSelectedCategory(item)}
-                    >
-                        {item}
-                    </div>
-                ))}
+                {
+                    options.map((item, index) => (
+                        <div
+                            key={index}
+                            className={`homePage_option ${selectedCategory === item ? 'selected' : ''}`}
+                            onClick={() => setSelectedCategory(item)}
+                        >
+                            {item}
+                        </div>
+                    ))
+                }
             </div>
 
             <div className={sideNavbar ? "home_mainPage" : "home_mainPageWithoutLink"}>
-                {/* Dummy data inline */}
-                {[
-                    {
-                        id: 1,
-                        title: "Learn JavaScript in 3 Hours",
-                        category: "JavaScript",
-                        thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
-                        duration: "3:10:47",
-                        user: "User1",
-                        likes: "3 Likes"
-                    },
-                    {
-                        id: 2,
-                        title: "React Crash Course",
-                        category: "React",
-                        thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
-                        duration: "2:45:12",
-                        user: "User2",
-                        likes: "5 Likes"
-                    },
-                    {
-                        id: 3,
-                        title: "CSS Basics",
-                        category: "CSS",
-                        thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
-                        duration: "1:15:30",
-                        user: "User3",
-                        likes: "2 Likes"
-                    },
-                    {
-                        id: 4,
-                        title: "HTML Beginner Guide",
-                        category: "HTML",
-                        thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
-                        duration: "1:00:00",
-                        user: "User4",
-                        likes: "4 Likes"
-                    },
-                    {
-                        id: 5,
-                        title: "Poetry Collection",
-                        category: "Poetry",
-                        thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg",
-                        duration: "0:45:00",
-                        user: "PoetX",
-                        likes: "10 Likes"
-                    }
-                ]
-                    .filter(video => selectedCategory === "All" || video.category === selectedCategory)
-                    .map(video => (
+                {
+                    filteredVideos.map((video) => (
                         <Link to={`/watch/${video.id}`} className="youtube_Video" key={video.id}>
                             <div className="youtube_thumbnailBox">
                                 <img src={video.thumbnail} alt="Thumbnail" className="youtube_thumbnailPic" />
@@ -247,11 +350,13 @@ function HomePage({ sideNavbar }) {
                                 </div>
                             </div>
                         </Link>
-                    ))}
+                    ))
+                }
             </div>
         </div>
     );
 }
 
 export default HomePage;
+
 
